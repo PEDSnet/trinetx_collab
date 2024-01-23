29a5662d72c0e466c8abd58cbd74ee478ceb5458
wieandk <wieandk@chop.edu>
2024-01-23 16:49:46 -0500

    add overall counts in addition to site specific counts

--- code/cohort_coverage_overlap.R
+++ code/cohort_coverage_overlap.R
@@ -2,16 +2,16 @@
 
 #' Check patient fact overlap between domains
 #'
-#' @param fact_tbls list of fact tables for the domains of interest. each list item should include the table in the first position
-#'                  and a string label for that patient group in the second position. 
-#'                  example can be found in `coverage_overlap_execute.R`
+#' @param fact_tbls list of fact tables for the domains of interest. each list item should include the 
+#'                  table in the first position and a string label for that patient group in the second 
+#'                  position. example can be found in `coverage_overlap_execute.R`
 #' @param check_string string to identify the check type
 #'
 #' @return one dataframe that summarises the count and proportion of patients in each overlapping group from the list of
 #'         fact tables. a patient is only counted in each group ONCE.
 #'         
-#'         denominator used to compute the proportion is the total number of patients that have at least one of the 
-#'         provided fact types (i.e. fact 1 OR fact 2 OR fact 3... etc)
+#'         denominator used to compute the proportion is the total number of patients that have at 
+#'         least one of the provided fact types (i.e. fact 1 OR fact 2 OR fact 3... etc)
 #'
 
 check_coverage_overlap <- function(fact_tbls,
@@ -19,16 +19,15 @@ check_coverage_overlap <- function(fact_tbls,
   
   grp_list <- list()
   
+  ## Loop through fact tables to identify patient groups
   for(i in 1:length(fact_tbls)){
-    
-    #tbl <- fact_tbls[[i]][[1]]
     
     label <- fact_tbls[[i]][[2]]
     
     tbl_meta <- fact_tbls[[i]][[1]] %>% 
       distinct(site, person_id) %>% 
       mutate(group = label) %>%
-      collect_new()
+      compute_new()
     
     grp_list[[i]] <- tbl_meta
   }
@@ -36,18 +35,38 @@ check_coverage_overlap <- function(fact_tbls,
   grp_reduce <- purrr::reduce(.x = grp_list,
                               .f = dplyr::union)
   
-  total_pts <- grp_reduce %>% group_by(site) %>% summarise(denom = n_distinct(person_id))
+  ## Overall and Site Total Counts
+  total_pts <- grp_reduce %>% summarise(denom_total = n_distinct(person_id)) %>%
+    compute_new()
   
+  total_site_pts <- grp_reduce %>% group_by(site) %>% summarise(denom_site = n_distinct(person_id)) %>%
+    compute_new()
+  
+  ## Collapse group labels per person_id
   grp_collapse <- grp_reduce %>% 
     arrange(site, group) %>%
     group_by(site, person_id) %>% 
-    summarise(group = str_c(group, collapse = '_')) %>%
-    group_by(site, group) %>%
-    summarise(n_grp = n())
+    summarise(group = str_flatten(group, collapse = '_')) %>%
+    ungroup() %>%
+    compute_new()
   
-  final_tbl <- grp_collapse %>%
+  ## Overall and Site Group Counts
+  summary_site <- grp_collapse %>%
+    group_by(site, group) %>%
+    summarise(n_grp_site = n()) 
+  
+  summary_total <- grp_collapse %>%
+    group_by(group) %>%
+    summarise(n_grp_total = n())
+  
+  ## Final Summary Table
+  final_tbl <- summary_site %>%
+    left_join(summary_total) %>%
+    left_join(total_site_pts) %>%
     left_join(total_pts) %>%
-    mutate(prop_pts = round(n_grp / denom, 2))
+    mutate(prop_pts = round(n_grp_site / denom_site, 2),
+           prop_total = round(n_grp_total / denom_total)) %>%
+    collect_new()
     
   return(final_tbl)
 }
